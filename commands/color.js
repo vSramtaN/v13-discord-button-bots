ff570ef4d3cfdac12ec9adb514db57dab91b1834
SrâmtaN <86779209+vSramtaN@users.noreply.github.com>
2022-04-15 18:29:25 +0300

    Create color.js

---
+++ commands/color.js
@@ -0,0 +1,10 @@
+module.exports = {
+    
+    name: 'color',
+  çalıştır: async (client, message, args,prefix ) => {
+    
+    const { MessageActionRow, MessageButton } = require('discord.js');
+    const babansramtanmaddox = require('../sramtan.json')
+    
+    
+    xxxxxxx 10 Star Sonra Sizlerle
